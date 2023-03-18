143b1dedfd34f76372c3b7610be1e1a84cf38be2
Samuel Victor <victorsamuelpraise@gmail.com>
2023-03-18 22:21:44 +0000

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	char c = 'a';
+
+		while (c < 'z')
+		{
+			if (c == 'e' || c == 'q')
+			{
+				c++;
+				continue;
+			}
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
