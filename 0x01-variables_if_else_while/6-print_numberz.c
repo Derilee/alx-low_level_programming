ef43d6eea6901fc6842ea83239d14ad12125cb10
Samuel Victor <victorsamuelpraise@gmail.com>
2023-03-18 21:35:32 +0000

    changed data type to char

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	int i = 0;
-	while(i < 10)
+	char i  = '0';
+	while(i <= '9')
 	{
 		putchar(i);
-		if (i != 10)
+		if (i != '9')
 		{
 			i++;
 		}
@@ -21,5 +21,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
