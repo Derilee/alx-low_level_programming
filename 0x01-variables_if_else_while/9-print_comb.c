490801c4d257a0fee42552390ce545141ab48bd7
Samuel Victor <victorsamuelpraise@gmail.com>
2023-03-18 22:46:02 +0000

    task 8 unfunished

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always(0) Success
+ */
+int main(void)
+{
+	int i = 0;
+	while (i <= 9)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			i++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
