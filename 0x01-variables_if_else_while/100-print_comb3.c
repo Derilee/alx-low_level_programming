fcb69680cf6e5df9327dae2b5da80eec9140a6fa
Samuel Victor <victorsamuelpraise@gmail.com>
2023-03-19 16:12:10 +0000

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always(0) Success
+ */
+int main(void)
+{
+	int i = 01;
+
+	while (i < 90)
+	{
+	       putchar(i + '01');
+	       if (i != 89)
+	       {
+		       putchar(',');
+		       putchar(' ');
+		       i++;
+	       }
+	       if else (i == 10 || i == 11 || i == 20 || i == 21 || i ==22 || i == 30 || i == 31 || i == 32 || i == 33 || i == 40 || i == 41 || i == 42 || i == 43 || i == 44
