90f3ebaae5134acc6cc738dfa776a610cf31ad0d
Samuel Victor <victorsamuelpraise@gmail.com>
2023-03-20 02:45:06 +0000

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,39 +9,23 @@ int main(void)
 {
 	int a = 0;
 	int b = 0;
-	int c = 0;
-	int d = 0;
 
-	while (a <= 9)
+	while (a < 100)
 	{
-		b = a + 1;
-		while (b <= 9)
+		while (b < 100)
 		{
-			c = b + 1;
-			while (c <= 9)
-			{
-				d = c + 1;
-				while (d <= 9)
-				{
-					if (a != b && b != c && c != d && a != c && a != d && b != d)
-					{
-						putchar(a + '0');
-							putchar(b + '0');
-							putchar(c + '0');
-							putchar(d + '0');
-					}
-					if ( a + b + c + d < 99)
-					{
-							putchar(',');
-							putchar(' ');
-					}
-							d++;
-				}
-				c++;
-			}
-			b++;
+		putchar((a/10) + '0');
+		putchar((a%10) + '0');
+		putchar(',');
+		putchar(' ');
+		putchar((b/10) + '0');
+		putchar((b%10) + '0');
+		putchar(',');
+		putchar(' ');
+		b++;
 		}
 		a++;
+	
 	}
 	putchar('\n');
 	return (0);
