84d5bfc74cf9959f69984c5a4c96f8662b92ec35
Samuel Victor <victorsamuelpraise@gmail.com>
2023-03-21 16:28:35 +0000

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define MAXSTRING 80
+/**
+ * main - Entry point
+ *
+ * Return: Always(0) success
+ */
+int main(void)
+{
+	int i;
+
+	i = '_';
+	putchar(i);
+	i = 'p';
+	putchar(i);
+	i = 'u';
+	putchar(i);
+	i = 't';
+	putchar(i);
+	i = 'c';
+	putchar(i);
+	i = 'h';
+	putchar(i);
+	i = 'a';
+	putchar(i);
+	i = 'r';
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory talks about C Functions and nested loops
