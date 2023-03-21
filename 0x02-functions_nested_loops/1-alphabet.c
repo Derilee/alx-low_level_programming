22600cf2af0eda03d24ddac84e63d9eb43ebd7d0
Samuel Victor <victorsamuelpraise@gmail.com>
2023-03-21 22:07:09 +0000

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Result: Always(0) Success
+ */
+
+/*void print_alphabet()
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+}*/
+int main(void)
+{
+	print_alphabet();
+	_putchar('\n');
+	return (0);
+}
