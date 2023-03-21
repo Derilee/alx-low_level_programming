e7394ed3925571eb143ff8191039ccaef602a9a5
Samuel Victor <victorsamuelpraise@gmail.com>
2023-03-21 21:50:44 +0000

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,13 +4,12 @@
  *
  * Return: Always(0) success
  */
-	
 int main(void)
 {
 	_putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
