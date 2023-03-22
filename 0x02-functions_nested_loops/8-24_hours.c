6ddc18d3fa8ac194310e064418205568fdc7053f
Samuel Victor <victorsamuelpraise@gmail.com>
2023-03-22 20:42:45 +0000

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
  * jack_bauer - function name
- * @i: variable name
  * Return: Always (0) Success
  */
 int jack_bauer(void)
 {
 	int i = 0;
 	int j;
-	
+
 	while (i < 24)
 	{
 		j = 0;
