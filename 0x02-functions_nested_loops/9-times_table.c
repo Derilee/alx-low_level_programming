9c880ab542e6df0eaa428e2aac8c69ec4a66881a
Samuel Victor <victorsamuelpraise@gmail.com>
2023-03-22 22:51:07 +0000

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,8 +1,44 @@
 #include "main.h"
 /**
  * times_table - function name
- * return: Always (0) Success
+ *
+ * Return: Always (0) Success
  */
 int times_table(void)
 {
-	int 
+	int i = 0;
+	int j;
+	int k;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			j++;
+			k = j * i;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}
+		i++;
+		_putchar('\n');
+	}
+	return (0);
+}
