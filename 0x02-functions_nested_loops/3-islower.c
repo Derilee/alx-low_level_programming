06e3dd0cd2ca48c1bf2411269f1932b57bc23f18
Samuel Victor <victorsamuelpraise@gmail.com>
2023-03-22 13:16:41 +0000

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ *c - Entry point
+ * Return: ALways(0) Success
+ */
+int _islower(int c)
+{
+	if (_islower(c))
+	{
+		return (1);
+	}
+		else
+		{
+			return (0);
+		}
+		_putchar('\n');
+}
