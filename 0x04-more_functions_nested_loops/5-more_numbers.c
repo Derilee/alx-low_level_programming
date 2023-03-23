93b6e5a0e0496425c852c8ac0bfe49fc6c29b999
Derilee <victorsamuelpraise@gmail.com>
2023-03-23 14:09:16 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,7 +5,7 @@
  */
 void more_numbers(void)
 {
-	int i = 0;
+	int i = 1;
 	int j;
 
 	while (i <= 10)
@@ -13,11 +13,11 @@ void more_numbers(void)
 		j = 0;
 		while (j <= 14)
 		{
-			j % 10;
-			_putchar(j + '0');
+			if (j >= 10)
+				_putchar('1');
+			_putchar(j % 10 + '0');
 			j++;
 		}
-		_putchar(i + '0');
 		_putchar('\n');
 		i++;
 	}
