4ad14ce8360da4da48b172db1a4225e015a03cb9
Samuel Victor <victorsamuelpraise@gmail.com>
2023-03-28 20:36:43 +0000

    swapping 2 values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - prototype that swaps values of two integers
+ * @a: value 1
+ * @b: value 2
+ * Return: Always 0
+ */
+void swap_int(int *a, int *b)
+{
+	a = b;
+	b = a;
+}
