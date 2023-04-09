87e221f59b52ac474ed900b17a6e30ef89e96024
Samuel Victor <victorsamuelpraise@gmail.com>
2023-04-09 15:05:43 +0000

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument counter that prints the number of argument passed into it
+ * @argv: argument vector
+ * Return: Always (0)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
