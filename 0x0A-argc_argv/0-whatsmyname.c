5883ac4de09426f80f0959928677f34699ab4afb
Samuel Victor <victorsamuelpraise@gmail.com>
2023-04-09 14:17:26 +0000

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: not used in this program
+ * @argv: argument vector used to print out the file name
+ * Return: Always (0)
+ */
 int main(int argc, char *argv[])
 {
 	(void)argc;
