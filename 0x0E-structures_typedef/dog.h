cd050ea4e6011e12b02e7fb99e8f3265358017b4
Samuel Victor <victorsamuelpraise@gmail.com>
2023-04-19 01:15:25 +0000

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
-typedef struct dog
+/**
+ * my_dog - to define a new type struct dog
+ */
+typedef struct dog my_dog;
+/**
+ * struct dog - stores the info of the dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
 {
 	char *name;
 	float *age;
 	char *owner;
-}
+};
 #endif
