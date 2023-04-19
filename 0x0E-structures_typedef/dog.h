c33fd67720215ed1717b0a5574cdbe507da20b3f
Samuel Victor <victorsamuelpraise@gmail.com>
2023-04-19 01:29:53 +0000

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,4 +16,9 @@ struct dog
 	float *age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void print_dog(struct dog *d);
+my_dog *new_dog(char *name, flaoat age, char *owner);
 #endif
