ed62f87207fe18ef8eb7e9e532f031feac7c24a2
Samuel Victor <victorsamuelpraise@gmail.com>
2023-04-19 03:22:01 +0000

    task 4 and 5

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * new_dog - creates new dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t->name = &name;
+	dog_t->age = age;
+	dog_t->owner = &owner;
+}
