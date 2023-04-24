fa7fd256fa31704f4889eb536d38bcf2c5336f2d
Samuel Victor <victorsamuelpraise@gmail.com>
2023-04-24 22:52:59 +0000

    task 2, fixed the conditional statement

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,14 +4,12 @@
  * @s: a string to be used
  * Return: length of the string
  */
-int _strlen_recurssion(char *s)
+int _strlen_recursion(char *s)
 {
 	int len = 0;
 
 	if (*s)
-	{
 		len++;
 		len += _strlen_recursion(s + 1);
-	}
 	return (len);
 }
