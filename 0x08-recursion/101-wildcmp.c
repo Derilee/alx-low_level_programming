99dcfa8a861d676e7f588dd75db1df7ac9727d2a
Samuel Victor <victorsamuelpraise@gmail.com>
2023-04-25 16:48:45 +0000

    task 8 recoded

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,82 +1,29 @@
 #include "main.h"
 /**
- * strlen_Xwildc - function that returns the length of a string,
- *			ignoring wildcard characters.
- * @str: The string to be measured.
- * Return: the length of a string
+ * wildcmp - A function to compare strings
+ * @s1: pointer to string params 1
+ * @s2: pointer to string params 2
+ * Return: 0
  */
-int strlen_Xwildc(char *str)
-{
-	int len = 0, i = 0;
 
-	if (*(str + i))
-	{
-		if (*str != '*')
-			len++;
-
-		i++;
-		len += strlen_Xwildc(str + i);
-	}
-	return (len);
-}
-
-/**
- * non_wildc - a function that iterates through a string located at
- *		a wildcard, until it points to a non-wildcard character.
- * @str: The string to be iterated through.
- */
-void non_wildc(char **str)
-{
-	if (**str == '*')
-	{
-		(*str)++;
-	}
-		non_wildc(str);
-}
-
-/**
- * str_match - function to check if a string matches another
- *		string containing wildcards.
- * @str: The string to be matched.
- * @poten_c: The variable
- * Return: a pointer to the null byte if str and poten_c are identical.
- *         or a pointer to the first unmatched character
- */
-char *str_match(char *str, char *poten_c)
-{
-	int len = strlen_Xwildc(str) - 1;
-	int poten_c_len = strlen_Xwildc(poten_c) - 1;
-
-	if (*poten_c == '*')
-		non_wildc(&poten_c);
-
-	if (*(str + len - poten_c_len) == *poten_c && *poten_c != '\0')
-	{
-		poten_c++;
-		return (str_match(str, poten_c));
-	}
-	return (poten_c);
-}
-
-/**
- * wildcmp - function that compares two strings,
- *		considering wildcard characters.
- * @s1: The first string to be compared.
- * @s2: The second string to be compared - may contain wildcards.
- * Return: If the strings can be considered identical - 1.
- *         Otherwise - 0.
- */
 int wildcmp(char *s1, char *s2)
 {
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+
 	if (*s2 == '*')
 	{
-		non_wildc(&s2);
-		s2 = str_match(s1, s2);
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
-	if (*s2 == '\0')
-		return (1);
-
-	if (*s1 != *s2)
-		return (0);
-	return (wildcmp(++s1, ++s2));
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
 }
