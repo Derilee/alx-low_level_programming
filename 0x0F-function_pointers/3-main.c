8e3398a654e5826be765a5ad366b3ee8a21ee525
Samuel Victor <victorsamuelpraise@gmail.com>
2023-04-30 01:07:38 +0000

    debugging task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -25,6 +25,12 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
+
+	if ((argv[1] == 0) || (argv[3] == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	printf("%d\n", op(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
