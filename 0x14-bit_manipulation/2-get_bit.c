65593e8897aa3c6d12270c16285119e00aa902e0
Samuel Victor <victorsamuelpraise@gmail.com>
2023-05-06 18:22:56 +0000

    fixed betty error

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,7 +13,9 @@ int get_bit(unsigned long int n, unsigned int index)
 	{
 		return (0);
 	}
-	 if (index >= (sizeof(unsigned long int) * 8))
-                return (-1);
-	return (1);
+	if (index >= (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+		return (1);
 }
